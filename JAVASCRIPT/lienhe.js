6dea6441964b96a66c670245319aea9ae32ded47
PhanTanHuy <phantanhuy265@gmail.com>
2023-12-08 17:02:04 +0700

    3/5 trang liên hệ đã xong

---
+++ JAVASCRIPT/lienhe.js
@@ -0,0 +1,28 @@
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+const slide = document.querySelectorAll('.hidden');
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showslideR');
+        } else {
+            entry.target.classList.remove('showslideR');
+        }
+    });
+});
+slide.forEach((el) => observer2.observe(el));
